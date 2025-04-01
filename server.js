0ebfedb7a72822909e2bd8ffbebe82fd1658877d
Chrónos <chronos.net@outlook.com>
2025-04-01 11:16:40 -0300

    get and post

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
+
+const moradores = []
+
 
 app.post('/moradores', async (req, res) => {
+
+    moradores.push(req.body)
+
+    res.status(201).json(moradores)
 })
 
 app.get('/moradores', async (req, res) => {
+
+    res.status(201).json(moradores)
 })
 
-app.put('/moradores/:id', async (req, res) => {
+/*app.put('/moradores/:id', async (req, res) => {
 })
 
 app.delete('/moradores/:id', async (req, res) => {
 })
+*/
 
 app.listen(3000)
