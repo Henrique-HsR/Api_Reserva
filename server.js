882898eef74b74194169331cc28b7aeb33714ae7
Chrónos <chronos.net@outlook.com>
2025-04-01 10:29:35 -0300

    estruturando serverjs

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express()
+
+app.post('/moradores', async (req, res) => {
+})
+
+app.get('/moradores', async (req, res) => {
+})
+
+app.put('/moradores/:id', async (req, res) => {
+})
+
+app.delete('/moradores/:id', async (req, res) => {
+})
+
+app.listen(3000)
